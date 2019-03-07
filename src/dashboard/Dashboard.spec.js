2704bb4c41937f95fac3706d08b4ce40759c53d1
joepound <14357797+joepound@users.noreply.github.com>
2019-03-07 08:39:23 -0700

    Dashboard render test + tests for Display component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
-// Test away
+import React from "react";
+
+import { render } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+import Dashboard from "./Dashboard";
+
+describe("\n Dashboard component render test:", () => {
+  it("• should render without crashing", () => {
+    render(<Dashboard />);
+  });
+});
